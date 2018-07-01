f8bb05b5de6cde6119e5473d0480b74eb8f9efe6
popdav <popdav@outlook.com>
2018-07-01 17:04:29 +0200

    new

--- client/src/Files.js
+++ client/src/Files.js
@@ -3,7 +3,7 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Cell from './Cell';
 import Pagination from "react-js-pagination";
-
+import axios from 'axios';
 
 class Files extends Component {
   constructor(props) {
@@ -38,13 +38,58 @@ class Files extends Component {
     return body;
   }
 
+  addToDb = () => {
+    let newBody = {
+      name: this.state.addIme,
+      last_name: this.state.addPrezime,
+      index: this.state.addIndeks
+    }
+
+    console.log(newBody);
+
+    axios.post('/add', newBody)
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
   render() {
     let styleT = {display: "block"};
     if(!this.props.showing)
       styleT = {display: "none"};
     const f = this.state.files;
+
+    let addForm = React.createRef();
+    function handleAddClick () {
+      addForm.current.style.display = "block";
+    }
+
+    function handleOutForm () {
+      addForm.current.style.display = "none";
+    }
+
+
     return (
-      <div style={styleT}>
+      <div style={styleT} className="FilesTable">
+
+        <button onClick={handleAddClick}>Add file</button>
+
+        <div className={'modal'} ref={addForm} onClick={handleOutForm}>
+            <form className={'modal-content'}>
+              <div className={"form-group"}>
+                <label>
+                  File path: <input type="text" className={"form-control"} name="addIme" onChange={this.handelAddFormInput}/>
+                </label>
+              </div>
+              <input type="submit" value="Submit" className={'btn btn-danger btnSubmit'} onClick={this.addToDb}/>
+            </form>
+        </div>
+
+
+
         <table className="table  table-style table-hover FilesTable" style={styleT}>
           <tbody><tr><th>Files:</th></tr></tbody>
           {
--- server.js
+++ server.js
@@ -24,55 +24,15 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 app.use(methodOverride());
 
-MongoClient.connect("mongodb://localhost", (err, client) => {
+let fileListDb = null;
+
+MongoClient.connect("mongodb://localhost:27017", { useNewUrlParser: true },(err, client) => {
   if(err)
     throw err;
 
-    bigDataDb = client.db('bigData');
-    gridfs = Grid(bigDataDb, Mongo);
-    bigDataDb.collection('fs.files').find().toArray(function(err, files) {
-      if (err)
-        throw err;
-
-      filesArr = files;
-    });
+  fileListDb = client.db("fileListDb");
+    
 })
 
-app.get('/files', (req, res) => {
-  res.send(filesArr);
-})
-
-app.post('/fileData', (req, res) => {
-  let fName = req.body.name;
-  let file = fileNameSearch(filesArr, fName);
-  gridfs.readFile(file.filename, (err, data) => {
-    if (err)
-      throw err;
-      
-    let stringToJSON = JSON.parse(data);
-    res.send(stringToJSON);
-  })
-
-  // let readStream = gridfs.createReadStream({
-  //   _id: filesArr[0]._id,
-  //   range: {
-  //     startPos: 0,
-  //     endPos: 10
-  //   }
-  // })
-  // readStream.on('data', (chunk) => {
-  //   console.log(chunk);
-  // })
-  //   res.send(buff);
-
-
-})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
-function fileNameSearch(arr = [],  name) {
-  for (let i = 0; i < arr.length; i++) {
-    if(arr[i].filename === name)
-      return arr[i];
-  }
-}
